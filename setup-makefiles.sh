c0f7c2a5c05ad2c8bbaecf0d46385e6a68d5a712
smokey18 <kmboplays@gmail.com>
2020-05-06 11:12:27 +0000

    Let's Ancient-ify...
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017, 2019 The LineageOS Project
+# Copyright (C) 2017, 2019 The AncientOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,9 +28,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ANCIENT_ROOT="${MY_DIR}/../../.."
 
-HELPER="$LINEAGE_ROOT/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="$ANCIENT_ROOT/vendor/ancient/build/tools/extract_utils.sh"
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -38,7 +38,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANCIENT_ROOT}"
 
 # Copyright headers and guards
 write_headers
